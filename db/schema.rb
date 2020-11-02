10affab6244efb91bec4472778140fcbc61a4d7d
muzz00ka <rorymuzz@gmail.com>
2020-11-02 20:33:54 +1100

    removed the wrong column from the address table. Added in the plural for the has many association too.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_085237) do
+ActiveRecord::Schema.define(version: 2020_11_02_092905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_11_02_085237) do
     t.integer "street_number"
     t.string "street_name"
     t.string "postcode"
-    t.string "integer"
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
