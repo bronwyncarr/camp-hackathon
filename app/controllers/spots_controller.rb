95dd89326b1ccbd095f12474429c6b98d7fd660f
Bronwyn Carr <bronwynacarr@gmail.com>
2020-11-03 13:57:42 +1100

    updated spot controller and created global styles

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,7 +1,16 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: [:show, :edit, :update, :destroy]
+
   def index
     @spots = Spot.all
   end
 
-  def show; end
+  def show;
+  end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_spot
+      @spot = Spot.find(params[:id])
+    end
 end
